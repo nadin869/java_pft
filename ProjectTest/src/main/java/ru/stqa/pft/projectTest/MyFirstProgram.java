6a6f231a262188e27fd983fa574e9c668b73f09c
nadin869 <nady86.86@mail.ru>
2019-06-20 14:54:17 +0300

    добавлено несколько функций

--- ProjectTest/src/main/java/ru/stqa/pft/projectTest/MyFirstProgram.java
+++ ProjectTest/src/main/java/ru/stqa/pft/projectTest/MyFirstProgram.java
@@ -3,8 +3,28 @@ package ru.stqa.pft.projectTest;
 public class MyFirstProgram {
 	
 	public static void main(String[] args) {
-	
-System.out.println("Hello, World!");
+	 hello("World");
+		hello("User");
+		hello("Nadya");
+
+		double length = 5;
+		double a = 2;
+		double b = 5;
+		System.out.println("Площадь квадрата = " + area(length));
+
+		System.out.println("Площадь квадрата = " + area(a, b));
+	}
+
+	public static void hello(String somebody){
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area (double l){
+		return l * l;
+	}
+
+	public static double area (double a, double b){
+		return a * b;
 	}
 
 }
